f92e2985c7083257630a72ba505a21e21a000b82
Abdurrahman Aydın <chatlaks_1992@hotmail.com>
2023-07-26 00:06:58 +0300

    Update unfollowall.js
    her takip bırakma arasında 30 saniye ve her 100 kişide bir random bekleme kısmı oluşturuldu

--- tools/unfollowall.js
+++ tools/unfollowall.js
@@ -14,35 +14,22 @@ const { chalk, inquirer, _, fs, instagram, print, delay } = require("./index.js"
            : TESTED "OK" BUG? YouTellMe!
     }`
     );
-    const questions = [
-        {
-            type: "input",
-            name: "username",
-            message: "Input username:",
-            validate: (val) => val.length != 0 || "Please input username!",
-        },
-        {
-            type: "password",
-            name: "password",
-            mask: "*",
-            message: "Input password:",
-            validate: (val) => val.length != 0 || "Please input password!",
-        },
-        {
-            type: "input",
-            name: "perExec",
-            message: "Input limit per-execution:",
-            validate: (val) => /[0-9]/.test(val) || "Only input numbers",
-        },
-        {
-            type: "input",
-            name: "delayTime",
-            message: "Input delay time (in milliseconds):",
-            validate: (val) => /[0-9]/.test(val) || "Only input numbers",
-        },
-    ];
     try {
-        const { username, password, target, perExec, delayTime } = await inquirer.prompt(questions);
+        const { username, password } = await inquirer.prompt([
+            {
+                type: "input",
+                name: "username",
+                message: "Input username:",
+                validate: (val) => val.length !== 0 || "Please input username!",
+            },
+            {
+                type: "password",
+                name: "password",
+                mask: "*",
+                message: "Input password:",
+                validate: (val) => val.length !== 0 || "Please input password!",
+            },
+        ]);
         const ig = new instagram(username, password);
         print("Try to Login . . .", "wait", true);
         const login = await ig.login(),
@@ -51,19 +38,21 @@ const { chalk, inquirer, _, fs, instagram, print, delay } = require("./index.js"
         print(`Collecting followed users . . .`, "wait");
         print(`You're following ${info.following_count} users!`, "ok");
         const following = await ig.followingFeed();
-        print(`Doing task with ratio ${perExec} target / ${delayTime} milliseconds \n`, "wait");
+        print(`Doing task with ratio 1 target / 30 seconds \n`, "wait");
+        let count = 0;
         do {
             let items = await following.items();
-            items = _.chunk(items, perExec);
             for (let i = 0; i < items.length; i++) {
-                await Promise.all(
-                    items[i].map(async (user) => {
-                        const unfollow = await ig.unfollow(user.pk);
-                        print(`▲ @${user.username} ⇶ ${unfollow ? chalk.bold.green("Unfollowed!") : chalk.bold.red("Failed to Unfollow!")}`);
-                    })
-                );
-                if (i < items.length - 1) print(`Current Account: (${login.username}) » Delay: ${perExec}/${delayTime}ms \n`, "wait", true);
-                await delay(delayTime);
+                await delay(30 * 1000); // 30 saniye bekleme (30 saniye = 30 * 1000 milisaniye)
+                const user = items[i];
+                const unfollow = await ig.unfollow(user.pk);
+                print(`▲ @${user.username} ⇶ ${unfollow ? chalk.bold.green("Unfollowed!") : chalk.bold.red("Failed to Unfollow!")}`);
+                count++;
+                if (count % 100 === 0) {
+                    const randomMinutes = Math.floor(Math.random() * 60) + 1; // 1 ile 60 dakika arasında rastgele bir süre belirle
+                    print(`Reached ${count} unfollows. Taking a ${randomMinutes}-minute break...`, "wait", true);
+                    await delay(randomMinutes * 60 * 1000); // Rastgele dakika ara (dakika = dakika * 60 * 1000 milisaniye)
+                }
             }
         } while (following.moreAvailable);
         print(`Status: All Task done!`, "ok", true);
@@ -71,4 +60,3 @@ const { chalk, inquirer, _, fs, instagram, print, delay } = require("./index.js"
         print(err, "err");
     }
 })();
-//by 1dcea8095a18ac73b764c19e40644b52 116 111 111 108 115 105 103  118 51
